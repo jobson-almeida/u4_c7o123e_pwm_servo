907717ddcf7ff3b73a9732321bd36ff409b77679
Jobson Almeida <jobson.ra@outlook.com>
2025-02-11 21:54:08 -0300

    chore: adicionar calculos para fins de experimentação e debugging

--- pwm_servo.c
+++ pwm_servo.c
@@ -3,17 +3,16 @@
 #include "hardware/pwm.h"
 #include "hardware/clocks.h"
 
-
 #define SERVO_GPIO 22
 #define FREQ_PWM 50.0f
 
 int main()
 {
     stdio_init_all();
- 
-    gpio_set_function(SERVO_GPIO, GPIO_FUNC_PWM); // define a função do pino 22 como saida PWM
+
+    gpio_set_function(SERVO_GPIO, GPIO_FUNC_PWM);       // define a função do pino 22 como saida PWM
     uint slice_num = pwm_gpio_to_slice_num(SERVO_GPIO); // determina a slice que controla a GPIO recém definida
-    float freq_clk_sys = clock_get_hz(clk_sys); // retorna a frequencia de clock do sistema (Hz)
+    float freq_clk_sys = clock_get_hz(clk_sys);         // retorna a frequencia de clock do sistema (Hz)
 
     // contatdor - também conhecimo com o TOP
     uint16_t wrap = 58304; // - melhor ajuste até o momento ajustado e experimentado
@@ -21,8 +20,18 @@ int main()
     float divider = freq_clk_sys / (50.0f * (wrap + 1)); // define frequência de 50Hz e o divisor do clock
     pwm_set_clkdiv(slice_num, divider);                  // aplica o divisor
     pwm_set_wrap(slice_num, wrap);                       // aplica o wrap
-    pwm_set_enabled(slice_num, true);  
-    
+    pwm_set_enabled(slice_num, true);
+
+    // DEBUGGING
+    // printf("%d\n", wrap);
+    // printf("%f\n", divider);
+
+    // uint16_t freq_pwm_calculate = freq_clk_sys / ((divider + 0 / 16) * wrap); DEBUGGING
+    // printf("%d\n", freq_pwm_calculate);
+
+    // uint16_t wrap_calculate = (freq_clk_sys * (divider + 0 / 16)) / FREQ_PWM; // DEBBUGING: valor funcional mas não ajustado
+    // printf("%d\n", wrap_calculate);
+
     while (true)
     {
         printf("Hello, world!\n");
